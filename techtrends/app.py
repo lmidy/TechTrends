c992de102fc869ecbcac83a4ebbb445890757013
lmidy <lynette.midy@gmail.com>
2022-02-07 22:38:57 -0600

    redo formatter

--- techtrends/app.py
+++ techtrends/app.py
@@ -109,6 +109,5 @@ def metrics():
 
 # start the application on port 3111
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    logging.basicConfig(format='%(asctime)s, %(message)s', datefmt='%m/%d/%Y, %I:%M:%S')
+    logging.basicConfig(format='%(asctime)s, %(message)s', datefmt='%m/%d/%Y, %I:%M:%S', level=logging.DEBUG)
     app.run(host='0.0.0.0', port='3111')
